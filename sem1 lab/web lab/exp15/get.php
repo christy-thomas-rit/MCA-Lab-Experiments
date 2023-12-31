a45122a42d4e640a497feb373293884282b8d612
christy-thomas-rit <148941878+christy-thomas-rit@users.noreply.github.com>
2023-12-31 23:04:22 +0530

    Add files via upload

---
+++ sem1 lab/web lab/exp15/get.php
@@ -0,0 +1,15 @@
+<?php
+if($_GET)
+{
+$number=$_GET["number"];
+if($number%2==0)
+{
+echo"$number is an even number";
+}else if($no==0)
+{
+echo "$number is neither odd or even";
+}else{
+echo "$number is an odd number";
+}
+}
+?>
